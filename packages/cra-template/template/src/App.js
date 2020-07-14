7c6293c751d380c37d85173912eb04afe2b0f94e
amuthan <amuthan.meganathan@techwave.net>
2020-07-14 20:33:15 +0530

    template changes

--- packages/cra-template/template/src/App.js
+++ packages/cra-template/template/src/App.js
@@ -23,7 +23,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to Unicode Labs</h2>
+          <h2>Welcome to Displaytree Labs</h2>
         </div>
         <p className="App-intro">
            <b> Employee ID: { employeeID } </b><br/><br/>
